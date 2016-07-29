d93266f945a9d7869e3c9b53957cd963ea4625ed
BigGood <srwl181818@gmail.com>
2016-07-29 18:36:08 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -84,9 +84,16 @@ synckey='|'.join(
 
 ToUserName=""
 for contact in jsondata['ContactList']:
-    if contact['NickName']=='拒绝黄赌毒':
+#     if contact['NickName']=='拒绝黄赌毒':
+#         ToUserName=contact['UserName']
+#         break;
+    if contact['NickName']=='文件传输助手':
         ToUserName=contact['UserName']
+        print("发送给"+ToUserName)
         break;
+
+
+
 xiaobingId=''
     
 while 1==1:
@@ -118,30 +125,94 @@ while 1==1:
 #     initdata = urllib.request.urlopen(req).read().decode('UTF-8')
 #     print(json.loads(initdata))
 
+SyncKey=jsondata['SyncKey']
+syncKeyStr='|'.join(
+                [str(keyVal['Key']) + '_' + str(keyVal['Val']) for keyVal in SyncKey['List']])
+
+#线路测试
+SyncHost = [
+            'webpush.weixin.qq.com',
+            'webpush2.weixin.qq.com',
+            'webpush.wechat.com',
+            'webpush1.wechat.com',
+            'webpush2.wechat.com',
+            'webpush1.wechatapp.com',
+            # 'webpush.wechatapp.com'
+        ]
+for hosts in SyncHost:
+    r=int(time.time())
+    params = { 
+        'r': r, 
+        'sid': sid ,
+        'uin': int(uin) ,
+        'skey': skey ,
+        'deviceid': DeviceId ,
+        'synckey': syncKeyStr ,
+        '_': int(time.time())
+    }
+    str1=jsonformat.someutil().toJson(params,"")
+#     print(str1)
+    req = urllib.request.Request('https://'+hosts+'/cgi-bin/mmwebwx-bin/synccheck?r=%s&skey=%s&sid=%s&synckey=%s' % (r, skey,sid,syncKeyStr),str1.encode('UTF-8'))
+    req.add_header('ContentType','application/json; charset=UTF-8' ) 
+    initdata = urllib.request.urlopen(req).read().decode('UTF-8')
+    print(initdata)
+    if initdata[initdata.find("retcode:\"")+9:initdata.find("\",")]=="0":
+        SyncHost=hosts
+        break
 
 while 1==1:
+    r=int(time.time())
+    params = { 
+        'r': r, 
+        'sid': sid ,
+        'uin': int(uin) ,
+        'skey': skey ,
+        'deviceid': DeviceId ,
+        'synckey': syncKeyStr ,
+        '_': int(time.time())
+    }
+    str1=jsonformat.someutil().toJson(params,"")
+#     print(str1)
+    req = urllib.request.Request('https://'+SyncHost+'/cgi-bin/mmwebwx-bin/synccheck?r=%s&skey=%s&sid=%s&synckey=%s' % (r, skey,sid,syncKeyStr),str1.encode('UTF-8'))
+    req.add_header('ContentType','application/json; charset=UTF-8' ) 
+    initdata = urllib.request.urlopen(req).read().decode('UTF-8')
+    print(initdata)
+    
+    
+    
+    
+    
+    
+    
     params = { 
          'BaseRequest': { 'Uin': int(uin), 'Sid': sid, 'Skey': skey, 'DeviceID': DeviceId }, 
-         'SyncKey': jsondata['SyncKey'], 
+         'SyncKey': SyncKey, 
          'rr': ~int(time.time())
     }
     str1=jsonformat.someutil().toJson(params,"")
-    print(str1)
+#     print(str1)
     req = urllib.request.Request('https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=%s&skey=%s&lang=zh_CN&pass_ticket=%s' % (sid, skey,pass_ticket),str1.encode('UTF-8'))
     req.add_header('ContentType','application/json; charset=UTF-8' ) 
     initdata = urllib.request.urlopen(req).read().decode('UTF-8')
     SyncKey=json.loads(initdata)['SyncKey']
+    syncKeyStr='|'.join(
+                [str(keyVal['Key']) + '_' + str(keyVal['Val']) for keyVal in SyncKey['List']])
+    print(json.loads(initdata))
+    
+    
     
     randomNum=str(int(time.time() * 1000)) + \
                 str(random.random())[:5].replace('.', '')
     for msgAdd in json.loads(initdata)['AddMsgList']:
-        if msgAdd['FromUserName']==ToUserName:
+#别人给自己发送         
+#         if msgAdd['FromUserName']==ToUserName:
+        if msgAdd['FromUserName']==jsondata['User']['UserName'] and msgAdd['MsgType']==1:
             print('向小冰发送'+msgAdd['Content'])
             params = { 
                       'BaseRequest': { 'Uin': int(uin), 'Sid': sid, 'Skey': skey, 'DeviceID': DeviceId }, 
                     'Msg': { 
                          'Type': 1 , 
-                         'Content': msgAdd['Content'], 
+                         'Content': msgAdd['Content'].replace('&lt;', '<').replace('&gt;', '>'), 
                          'FromUserName': jsondata['User']['UserName'], 
                          'ToUserName': xiaobingId, 
                          'LocalID': randomNum, 
@@ -152,8 +223,29 @@ while 1==1:
             req = urllib.request.Request("https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsg?pass_ticket=%s" % (pass_ticket),str1.encode('UTF-8'))
             req.add_header('ContentType','application/json; charset=UTF-8' ) 
             initdata = urllib.request.urlopen(req).read().decode('UTF-8')
+            if json.loads(initdata)['BaseResponse']['Ret']==0:
+                print("向"+xiaobingId+"发送"+ msgAdd['Content']+"成功")
+            
             
     time.sleep(3)
+    
+    r=int(time.time())
+    params = { 
+        'r': r, 
+        'sid': sid ,
+        'uin': int(uin) ,
+        'skey': skey ,
+        'deviceid': DeviceId ,
+        'synckey': syncKeyStr ,
+        '_': int(time.time())
+    }
+    str1=jsonformat.someutil().toJson(params,"")
+#     print(str1)
+    req = urllib.request.Request('https://webpush.wx.qq.com/cgi-bin/mmwebwx-bin/synccheck?r=%s&skey=%s&sid=%s&synckey=%s' % (r, skey,sid,syncKeyStr),str1.encode('UTF-8'))
+    req.add_header('ContentType','application/json; charset=UTF-8' ) 
+    initdata = urllib.request.urlopen(req).read().decode('UTF-8')
+    print(initdata)
+    
             
     params = { 
          'BaseRequest': { 'Uin': int(uin), 'Sid': sid, 'Skey': skey, 'DeviceID': DeviceId }, 
@@ -165,9 +257,10 @@ while 1==1:
     req = urllib.request.Request('https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=%s&skey=%s&lang=zh_CN&pass_ticket=%s' % (sid, skey,pass_ticket),str1.encode('UTF-8'))
     req.add_header('ContentType','application/json; charset=UTF-8' ) 
     initdata = urllib.request.urlopen(req).read().decode('UTF-8')
-#     print(json.loads(initdata))
+    
+    SyncKey=json.loads(initdata)['SyncKey']
     for msgAdd in json.loads(initdata)['AddMsgList']:
-        if msgAdd['FromUserName']==xiaobingId:
+        if msgAdd['FromUserName']==xiaobingId and msgAdd['MsgType']==1:
             print("小冰回复："+msgAdd['Content'])
             params = { 
                       'BaseRequest': { 'Uin': int(uin), 'Sid': sid, 'Skey': skey, 'DeviceID': DeviceId }, 
@@ -184,6 +277,8 @@ while 1==1:
             req = urllib.request.Request("https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsg?pass_ticket=%s" % (pass_ticket),str1.encode('UTF-8'))
             req.add_header('ContentType','application/json; charset=UTF-8' ) 
             initdata = urllib.request.urlopen(req).read().decode('UTF-8')
+            if json.loads(initdata)['BaseResponse']['Ret']==0:
+                print("向"+ToUserName+"发送"+ msgAdd['Content']+"成功")
     
 #     sendMsg = input();
 #     randomNum=str(int(time.time() * 1000)) + \
